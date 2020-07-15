b20926491a673a728ede81b9313277c1837d6aad
pdiazmol <pablodiazmolina07@gmail.com>
2020-07-15 09:16:59 +0200

    Agregado método vacio para realizar las operaciones

--- calculadora/src/main/java/com/sanitas/calculadora/services/impl/CalculadoraServiceImpl.java
+++ calculadora/src/main/java/com/sanitas/calculadora/services/impl/CalculadoraServiceImpl.java
@@ -1,7 +1,14 @@
 package com.sanitas.calculadora.services.impl;
 
+import com.sanitas.calculadora.services.ICalculadoraService;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
-public class CalculadoraServiceImpl {
+public class CalculadoraServiceImpl implements ICalculadoraService {
+    @Override
+    public double realizarOperacion(BigDecimal numero1, BigDecimal numero2, String operacion) {
+        return 0;
+    }
 }
