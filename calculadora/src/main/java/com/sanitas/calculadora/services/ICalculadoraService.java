361d33fdb87545fdcbd624135b7dfb0dc06edf4d
pdiazmol <pablodiazmolina07@gmail.com>
2020-07-15 09:14:00 +0200

    Agregado método para realizar la operación a la interfaz del servicio

--- calculadora/src/main/java/com/sanitas/calculadora/services/ICalculadoraService.java
+++ calculadora/src/main/java/com/sanitas/calculadora/services/ICalculadoraService.java
@@ -1,4 +1,7 @@
 package com.sanitas.calculadora.services;
 
+import java.math.BigDecimal;
+
 public interface ICalculadoraService {
+    double realizarOperacion (BigDecimal numero1, BigDecimal numero2, String operacion);
 }
