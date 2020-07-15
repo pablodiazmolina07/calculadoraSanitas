bd5d71db33e4f447d1a9a096cc9ab2354084e79b
pdiazmol <pablodiazmolina07@gmail.com>
2020-07-15 09:06:52 +0200

    Constantes para las operaciones de suma y resta

--- calculadora/src/main/java/com/sanitas/calculadora/constants/Constantes.java
+++ calculadora/src/main/java/com/sanitas/calculadora/constants/Constantes.java
@@ -1,4 +1,9 @@
 package com.sanitas.calculadora.constants;
 
 public class Constantes {
+    // Constantes para la operación suma
+    public static final String OPERACION_SUMA = "SUMA";
+
+    // Constantes para la operación resta
+    public static final String OPERACION_RESTA = "RESTA";
 }
